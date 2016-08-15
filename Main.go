4a5ce35e65da9a71a61d9af8a6f004d93b848862
WillemElbers <willem@clarin.eu>
2016-08-15 16:47:10 +0200

    Added config options via env parameters, centralised loading of env parameters, refactored log output to a single line per invocation

--- Main.go
+++ Main.go
@@ -6,6 +6,7 @@ import (
     	"fmt"
 	"os"
 	"strings"
+	"strconv"
 	"bytes"
 	"time"
 	"encoding/json"
@@ -16,6 +17,20 @@ import (
 	"crypto/tls"
 	"launchpad.net/xmlpath"
 )
+
+const key_shib_assertion_count = "Shib_Assertion_Count"
+const key_shib_assertion = "Shib_Assertion_"
+const key_config_submit_stats = "submit_sp_stats"
+const key_config_log_path = "log_path"
+const key_config_log_file = "log_file"
+const key_config_aag_url = "aag_url"
+const key_config_aag_path = "aag_path"
+
+const default_log_path = "/var/log/sp-session-hook/"
+const default_log_file = "session-hook-golang.log"
+const default_aggregator_url = "https://clarin-aa.ms.mff.cuni.cz"
+const default_aggregator_path = "/aaggreg/v1/got"
+const default_submitSpStats = false
 
 type apiResponse struct {
     ok bool `json:"ok"`
@@ -97,20 +112,20 @@ func getAttributeAssertions(url string) (*attributeInfo, error) {
 }
 
 func getShibbolethAssertionUrl() (string, error) {
-	_shibAssertionCount := os.Getenv("Shib_Assertion_Count")
+	_shibAssertionCount := os.Getenv(key_shib_assertion_count)
 	if(len(_shibAssertionCount) == 0) {
-		return "", errors.New("Shib-Assertion-Count variable not found")
+		return "", errors.New(key_shib_assertion_count+" variable not found")
 	}
 
-	_shibAssertion := os.Getenv("Shib_Assertion_"+_shibAssertionCount)
+	_shibAssertion := os.Getenv(key_shib_assertion+_shibAssertionCount)
 	if(len(_shibAssertion) == 0) {
-		return "", errors.New("Shib_Assertion_"+_shibAssertionCount+" variable not found")
+		return "", errors.New(key_shib_assertion+_shibAssertionCount+" variable not found")
 	}
 
 	return strings.Replace(_shibAssertion, "localhost", "127.0.0.1", 1), nil
 }
 
-func sendInfo(aggregator_url string, aggregator_path string, aInfo *attributeInfo) error {
+func sendInfo(submit bool, aggregator_url string, aggregator_path string, aInfo *attributeInfo) error {
 	aagUrl, err := url.Parse(aggregator_url)
     	if err != nil {
         	return err
@@ -129,33 +144,37 @@ func sendInfo(aggregator_url string, aggregator_path string, aInfo *attributeInf
     	}
 	aagUrl.RawQuery = parameters.Encode()
 
-	logInfo(aagUrl.String())
+	if(submit) {
+		logInfo(fmt.Sprintf("Submitting statistics: %s", aagUrl.String()))
+		client := &http.Client{
+			Timeout: time.Second * 10,
+		}
 
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
+		response, err := client.Get(aagUrl.String())
+		if err != nil {
+			return err
+		}
 
-	response, err := client.Get(aagUrl.String())
-	if err != nil {
-		return err
-	}
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return err
+		}
 
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-	   	return err
-	}
+		var s = new(apiResponse)
+		err = json.Unmarshal(body, &s)
+		if (err != nil) {
+			return err
+		}
 
-	var s = new(apiResponse)
-	err = json.Unmarshal(body, &s)
-	if(err != nil){
-		return err
-	}
-
-	if(s.ok) {
-		logInfo("Success")
+		if (s.ok) {
+			logInfo("Success")
+		} else {
+			logInfo(fmt.Sprintf("Failed: [%v]", body))
+		}
 	} else {
-		logInfo(fmt.Sprintf("Failed: [%v]", body))
+		logInfo(fmt.Sprintf("Submission of statistics is disabled: %s", aagUrl.String()))
 	}
+
 	return nil
 }
 
@@ -165,22 +184,50 @@ func logInfo(msg string) {
 
 func logError(msg string) {
 	log.Printf("[ERROR] %s", msg)
+}
+
+func logErrorWithResponse(msg string) {
+	logError(msg)
 	sendErrorResponse(500, msg)
 }
 
+func getStringConfigValue(key string, defaultValue string) (string) {
+	value := os.Getenv(key)
+	if len(value) > 0 {
+		return value
+	} else {
+		return defaultValue
+	}
+}
+
+func getBooleanConfigValue(key string, defaultValue bool) (bool) {
+	value := os.Getenv(key)
+	if len(value) > 0 {
+		x, err := strconv.ParseBool(value)
+		if err != nil {
+			logError(fmt.Sprintf("error parsing %s value to bool: %v", key, err))
+			return false
+		}
+		return x
+	} else {
+		return defaultValue
+	}
+}
 
 func main() {
-	//TODO: make these parameters configurable. Use environment variables?
 	//TODO: log errors to file and provide more generic error messages
-	//TODO: verify aagregator url, and timestamp format specifically, with Jozef before actually sending information over the wire
-	log_file := "/var/log/sp-session-hook/session-hook-golang.log"
-	aggregator_url := "https://clarin-aa.ms.mff.cuni.cz"
-	aggregator_path := "/aaggreg/v1/got"
 
-	//Initialize logging
-	f, err := os.OpenFile(log_file, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	//Parse configuration options
+	log_path := getStringConfigValue(key_config_log_path, default_log_path)
+	log_file := getStringConfigValue(key_config_log_file, default_log_file)
+	aggregator_url := getStringConfigValue(key_config_aag_url, default_aggregator_url)
+	aggregator_path := getStringConfigValue(key_config_aag_path, default_aggregator_path)
+	submitSpStats := getBooleanConfigValue(key_config_submit_stats, default_submitSpStats)
+
+	//Switch to file based logging
+	f, err := os.OpenFile(fmt.Sprintf("%s%s", log_path, log_file), os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
 	if err != nil {
-		logError(fmt.Sprintf("error opening file: %v", err))
+		logError(fmt.Sprintf("failed to open logfile: %v", err))
 		return
 	}
 	defer f.Close()
@@ -189,7 +236,7 @@ func main() {
 	//Get url query parameters
     	req, err := cgi.Request()
     	if err != nil {
-        	logError("cannot get cgi request: " + err.Error())
+        	logErrorWithResponse("cannot get cgi request: " + err.Error())
        		return
     	}
 	_return := req.URL.Query().Get("return")
@@ -197,21 +244,21 @@ func main() {
 	//Get assertion url
 	_shibAssertionUrl, err := getShibbolethAssertionUrl()
 	if err != nil {
-		logError("Failed to parse shibboleth variables: " + err.Error())
+		logErrorWithResponse("Failed to parse shibboleth variables: " + err.Error())
 		return
 	}
 
 	//Get attribute assertions
 	attrInfo, err := getAttributeAssertions(_shibAssertionUrl)
 	if err != nil {
-		logError("Failed to parse shibboleth attribute assertions: " + err.Error())
+		logErrorWithResponse("Failed to parse shibboleth attribute assertions: " + err.Error())
 		return
 	}
 
 	//Send info to aagregator
-	errSendInfo := sendInfo(aggregator_url, aggregator_path, attrInfo)
+	errSendInfo := sendInfo(submitSpStats, aggregator_url, aggregator_path, attrInfo)
 	if errSendInfo != nil {
-		logError("Failed to send attribute information to aagregator: " + errSendInfo.Error())
+		logErrorWithResponse("Failed to send attribute information to aagregator: " + errSendInfo.Error())
 		return
 	}
 
